a449779d907824dd8da87d8bc996b7c5f27ee4db
Catalin Badea <badeacatalin1981@yahoo.com>
2026-07-25 19:44:58 +0300

    Fix capitalization in comment for CPUID 0x306C3

--- keep_core0_free_auto_cs2.ps1
+++ keep_core0_free_auto_cs2.ps1
@@ -1,7 +1,7 @@
 # =========================================================================
 # 1. Fortare drepturi de Administrator (UAC Auto-Elevate)
 # powershell.exe -ExecutionPolicy Bypass -File "C:\Tools\keep_core0_free_auto_cs2.ps1"
-# Adaugare mod Volt2 0.945/0.946 pentru CPUID 0x306c3
+# Adaugare mod Volt2 0.945/0.946 pentru CPUID 0x306C3
 # =========================================================================
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
